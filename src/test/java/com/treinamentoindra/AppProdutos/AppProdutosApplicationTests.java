10bcfe097b27e039c82e012c4570dbb30b16c243
beatrizalvs <bearizt5@gmail.com>
2023-10-29 16:12:23 -0300

    first commit

---
+++ src/test/java/com/treinamentoindra/AppProdutos/AppProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentoindra.AppProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
