644a5632e89e7f092a29ea6a0647a6c588cd5f5f
beatrizalvs <bearizt5@gmail.com>
2023-10-29 19:11:45 -0300

    create model

---
+++ src/main/java/com/treinamentoindra/AppProdutos/model/Produto.java
@@ -0,0 +1,82 @@
+package com.treinamentoindra.AppProdutos.model;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String codigoBarras;
+
+    @Column(nullable = false)
+    private String nomeProduto;
+
+    @Column(nullable = false)
+    private BigDecimal precoProduto;
+
+    public Produto(){}
+
+    public Produto(Long id, String codigoBarras, String nomeProduto, BigDecimal precoProduto){
+        this.id = id;
+        this.codigoBarras = codigoBarras;
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(BigDecimal precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        return Objects.equals(id, other.id);
+    }
+}
